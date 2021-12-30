b7f292e6f7b104d7231ae0f8b4f53ce9083b8eb9
William Wai Lim Chan <wchan365@uwo.ca>
2021-12-30 13:23:54 -0500

    fuck i suck ;(

---
+++ grokking/two pointers/subarray_product.py
@@ -0,0 +1,52 @@
+'''
+Problem Statement 
+
+Given an array with positive numbers and a target number, 
+find all of its contiguous subarrays whose product is less than the target number.
+
+Example 1:
+Input: [2, 5, 3, 10], target=30 
+Output: [2], [5], [2, 5], [3], [5, 3], [10]
+Explanation: There are six contiguous subarrays whose product is less than the target.
+
+2,3,2,1
+
+
+Example 2:
+Input: [8, 2, 6, 5], target=50 
+Output: [8], [2], [8, 2], [6], [2, 6], [5], [6, 5] 
+Explanation: There are seven contiguous subarrays whose product is less than the target.
+
+Approach
+- lol it says 2 pointer but i think it's a bit like sliding window
+
+TC: O(n^3)
+SC: O(n)
+'''
+
+from collections import deque
+
+def subarray(nums, target):
+    currProduct = 1
+    start = 0
+    res = []
+
+    # O(n)
+    for end, num in enumerate(nums):
+        currProduct *= num
+        
+        # O(1)
+        while currProduct >= target:
+            currProduct /= nums[start]
+            start += 1
+        
+        tmp = deque()
+        # O(n)
+        for i in range(end, start-1, -1):
+            tmp.appendleft(nums[i])
+            res.append(list(tmp))
+        print(tmp, res)
+    return res
+
+print(subarray([2,5,3,10], 30))
+

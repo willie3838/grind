a68a70b109769d643ed4f2c1056d345f004bf1b4
William Wai Lim Chan <wchan365@uwo.ca>
2022-02-05 21:20:31 -0500

    boom

--- grokking/merge intervals/insert_interval.py
+++ grokking/merge intervals/insert_interval.py
@@ -23,7 +23,7 @@ Explanation: After insertion, since [1,4] overlaps with [2,3], we merged them in
 def insert(intervals, newI):
     res = []
     for x,y in intervals:
-        if newI and newI[0] <= x:
+        if newI and newI[0] <= y:
             res.append(newI)
             newI = None
         if not res or x > res[-1][1]:
@@ -31,7 +31,9 @@ def insert(intervals, newI):
         else:
             res[-1][0] = min(x, res[-1][0])
             res[-1][1] = max(y, res[-1][1])
-    
+    if not res or newI:
+        res.append(newI)
+
     return res
 
 print(insert([[1,3], [5,7], [8,12]], [4,6]))

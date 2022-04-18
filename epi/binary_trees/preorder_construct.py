d5eb23f2cab623cdb3c0aca27d5043ef0d7f4580
William Wai Lim Chan <wchan365@uwo.ca>
2022-04-18 11:32:17 -0400

    im so smart

---
+++ epi/binary_trees/preorder_construct.py
@@ -0,0 +1,27 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+'''
+    1
+2       3
+
+preorder: [1,2,null,null,3,null,null]
+
+          1
+    2           3
+4       5   
+
+
+preorder: [1,2,4,null,null,5,null,null,3,null,null]
+'''
+def reconstruct(preorder):
+    root = preorder.pop(0)
+    if not root:
+        return root
+    root.left = reconstruct(preorder)
+    root.right = reconstruct(preorder)
+    return root

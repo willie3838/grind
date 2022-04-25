8599c253e0e656a2f4b8b2baf38198f110447c25
William Wai Lim Chan <wchan365@uwo.ca>
2022-04-25 11:24:33 -0400

    quick select understood

---
+++ epi/search/find_kth_largest.py
@@ -0,0 +1,34 @@
+import random
+
+'''
+[1,2,3], k = 2
+
+'''
+def findKthLargest(arr, k):
+    l,r = 0,len(arr)-1
+
+    while l <= r:
+        pivot = partition(arr, l, r)
+        if len(arr)-pivot == k:
+            return arr[pivot]
+        elif len(arr)- pivot > k:
+            l = pivot + 1
+        else:
+            r = pivot - 1
+    return None
+
+        
+
+
+def partition(arr, l, r):
+    pivot = random.randint(l, r)
+    arr[pivot], arr[r] = arr[r], arr[pivot]
+
+    i = l
+    for j in range(len(arr)):
+        if arr[j] < arr[pivot]:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    
+    arr[r], arr[i] = arr[i], arr[r]
+    return i
